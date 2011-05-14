e0c9a926560eae47a9c49acc49ef747dc52eb2ed
xaicron <xaicron@gmail.com>
2011-05-14 18:52:43 +0900

    initial commit

---
+++ lib/Xaicron/Dotcloud/Static.pm
@@ -0,0 +1,42 @@
+package Xaicron::Dotcloud::Static;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Xaicron::Dotcloud::Static -
+
+=head1 SYNOPSIS
+
+  use Xaicron::Dotcloud::Static;
+
+=head1 DESCRIPTION
+
+Xaicron::Dotcloud::Static is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
